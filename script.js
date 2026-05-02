760dd8e87d82c030d09403e45f2a6b7aea98e7f4
LexusLXR <68087923+LexusLXR@users.noreply.github.com>
2026-05-02 18:54:32 +0530

    phase 2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const images = [
     "images/Dental_chair_1.png",
-    "images/Dental_chair_2.png"
+    // "images/Dental_chair_2.png"
 ];
 
 let index = 0;
@@ -22,3 +22,35 @@ changeBackground();
 
 // change every 4 seconds
 setInterval(changeBackground, 4000);
+
+
+
+const slider = document.getElementById("slider");
+const afterImg = document.getElementById("afterImg");
+const container = document.getElementById("compare");
+
+let isDragging = false;
+
+slider.addEventListener("mousedown", () => {
+    isDragging = true;
+});
+
+window.addEventListener("mouseup", () => {
+    isDragging = false;
+});
+
+window.addEventListener("mousemove", (e) => {
+    if (!isDragging) return;
+
+    const rect = container.getBoundingClientRect();
+    let x = e.clientX - rect.left;
+
+    // limit inside container
+    x = Math.max(0, Math.min(x, rect.width));
+
+    // move slider
+    slider.style.left = x + "px";
+
+    // reveal after image
+    afterImg.style.width = x + "px";
+});
